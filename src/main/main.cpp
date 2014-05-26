7e5a546d69615ff8d298802bf98b6de182cb6bff
Łukasz Hanuszczak <mrhania.os@gmail.com>
2014-05-26 18:48:56 +0200

    Remove compilation error on case-sensitive systems. Fix segmentation fault bug.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -11,12 +11,14 @@ int main(int argc, char *argv[])
     Application application(argc, argv);
 
     MainWindow mainWindow;
+
     ToolActionArea actionArea;
     DragTool dragTool;
-    mainWindow.setCentralWidget(&actionArea);
-//    actionArea.setCanvas(application.project().canvas());
     actionArea.setTool(&dragTool);
 
+//    actionArea.setCanvas(application.project().canvas());
+
+    mainWindow.setCentralWidget(&actionArea);
     mainWindow.show();
 
     return application.exec();
