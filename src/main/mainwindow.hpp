2c4dd6d2d40204ec5822d08df5231ba2a1680f7f
Marcin Kaczmarek <mkacz91@gmail.com>
2014-06-07 14:56:52 +0200

    Added missing headers to ApricotView

--- src/main/mainwindow.hpp
+++ src/main/mainwindow.hpp
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include <ApricotView>
+
 namespace Ui {
 class MainWindow;
 }
@@ -17,6 +19,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+
 };
 
 #endif // MAINWINDOW_HPP
