182939afe442e021b50b6a05dbdaa21f9ee4e689
Łukasz Hanuszczak <mrhania.os@gmail.com>
2014-06-09 13:14:01 +0200

    Enhance Painter class with some new stuff.
    Painter now allows pixel manipulation using brush with given color and size.
    It also tracks bounding rect of all performed operations as mentioned in #7.

--- src/core/painter.cpp
+++ src/core/painter.cpp
@@ -12,6 +12,31 @@
  */
 
 /*!
+ * \fn const QRect &Painter::boundingBox() const
+ * \brief Returns bounding box of area changed by the drawing events.
+ */
+
+/*!
+ * \fn const QColor &Painter::brushColor() const
+ * \brief Returns current brush color.
+ */
+
+/*!
+ * \fn int Painter::brushSize() const
+ * \brief Returns current brush size.
+ */
+
+/*!
+ * \fn void Painter::setBrushColor(const QColor &color)
+ * \brief Changes current brush color to \a color.
+ */
+
+/*!
+ * \fn void Painter::setBrushSize(int size)
+ * \brief Changes current brush size to \a size.
+ */
+
+/*!
  * \brief Constructs a painter that paints on \a canvas.
  */
 Painter::Painter(Canvas &canvas) :
@@ -27,5 +52,28 @@ Painter::Painter(Canvas &canvas) :
  */
 void Painter::drawImage(QString fileName)
 {
-    painter->drawPixmap(0, 0, QPixmap(fileName));
+    QPixmap pixmap(fileName);
+
+    painter->drawPixmap(0, 0, pixmap);
+    m_boundingBox |= pixmap.rect();
 }
+
+/*!
+ * \brief Draws a point centered around \a x and \a y coordinates.
+ */
+void Painter::drawPoint(int x, int y)
+{
+    int size = brushSize();
+    QRect drawingRect(x - size / 2, y - size / 2, size, size);
+
+    // We use this instead of QPainter::drawPoint to avoid weird problems.
+    painter->fillRect(drawingRect, brushColor());
+    m_boundingBox |= drawingRect;
+}
+
+/*!
+ * \fn void Painter::drawPoint(const QPoint &position)
+ * \brief Draws a point centered around \a position.
+ *
+ * This is an overloaded function.
+ */
--- src/core/painter.hpp
+++ src/core/painter.hpp
@@ -13,9 +13,53 @@ public:
     explicit Painter(Canvas &canvas);
 
     void drawImage(QString fileName);
+    void drawPoint(int x, int y);
+    void drawPoint(const QPoint &position);
+
+    const QRect &boundingBox() const;
+    const QColor &brushColor() const;
+    int brushSize() const;
+
+    void setBrushColor(const QColor &color);
+    void setBrushSize(int size);
 
 private:
     std::shared_ptr<QPainter> painter;
+
+    QRect m_boundingBox;
+
+    QColor m_brushColor;
+    int m_brushSize;
 };
+
+inline void Painter::drawPoint(const QPoint &position)
+{
+    drawPoint(position.x(), position.y());
+}
+
+inline const QRect &Painter::boundingBox() const
+{
+    return m_boundingBox;
+}
+
+inline const QColor &Painter::brushColor() const
+{
+    return m_brushColor;
+}
+
+inline int Painter::brushSize() const
+{
+    return m_brushSize;
+}
+
+inline void Painter::setBrushColor(const QColor &color)
+{
+    m_brushColor = color;
+}
+
+inline void Painter::setBrushSize(int size)
+{
+    m_brushSize = size;
+}
 
 #endif // CORE_PAINTER_HPP
